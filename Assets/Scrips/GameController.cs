f1fa5efb1512d941a2ef376125ab6aac4954a9fb
Bryce <dbgtrulz@optonline.net>
2015-11-12 16:30:55 -0500

    Working on wave spawn

--- Assets/Scrips/GameController.cs
+++ Assets/Scrips/GameController.cs
@@ -1,38 +1,85 @@
 using UnityEngine;
 using System.Collections;
+using EZObjectPools;
 
 public class GameController : MonoBehaviour {
 	public static float Unit = 2.5f;
 	public GUIController GUI = null;
 
-	public int MaxEnemies = 25;
+	public int EnemiesPerWave = 200;
 	public Transform Player = null;
-	public Transform EnemyShipPrefab = null;
-	public Transform EnemyMechPrefab = null;
-	private Transform[] enemyList = null;
+	public GameObject EnemyShip1Prefab = null;
+	public GameObject EnemyShip2Prefab = null;
+	public GameObject EnemyShip3Prefab = null;
+	public GameObject EnemyMech1Prefab = null;
+	public GameObject EnemyMech2Prefab = null;
+	public GameObject EnemyMech3Prefab = null;
 	public Transform asteroid1Prefab = null;
 	public Transform asteroid2Prefab = null;
 	public Transform asteroid3Prefab = null;
 	public float asteroidRange;
-
+	
+	public float SpawnInterval;
+	private float spawnTimer;
+	public int MaxWaves;
+	private int currentWave = 0;
 
 	private bool inProgress = true;
 	private int totalSoulScore;
+	
+	private static class Enemy {
+		public const int Ship1 = 0;
+		public const int Mech1 = 1;
+		public const int Ship2 = 2;
+		public const int Mech2 = 3;
+		public const int Ship3 = 4;
+		public const int Mech3 = 5;
+	}
+	
+	private Wave[] waves;
 
 	// Use this for initialization
 	void Start () {
-		enemyList = new Transform[MaxEnemies];
-		Spawn ();
+		//Spawn ();
 		createAsteroidField ();
 		totalSoulScore = 0;
+		spawnTimer = 0;
+
+		waves = new Wave[MaxWaves];
+		int[] enemies0 = new int[6];
+		enemies0[Enemy.Ship1] = 20;
+		enemies0[Enemy.Ship2] = 0;
+		enemies0[Enemy.Ship3] = 0;
+		enemies0[Enemy.Mech1] = 0;
+		enemies0[Enemy.Mech2] = 0;
+		enemies0[Enemy.Mech3] = 0;
+		waves [0] = new Wave (this, 0, enemies0);
+
+		int[] enemies1 = new int[6];
+		enemies1[Enemy.Ship1] = 10;
+		enemies1[Enemy.Ship2] = 0;
+		enemies1[Enemy.Ship3] = 0;
+		enemies1[Enemy.Mech1] = 10;
+		enemies1[Enemy.Mech2] = 0;
+		enemies1[Enemy.Mech3] = 0;
+		waves [1] = new Wave (this, 1, enemies1);
 	}
 
-	void Spawn() {
-		for (int i=0; i<MaxEnemies; i++) {
-			Transform enemy = (Transform) Instantiate(EnemyShipPrefab, Player.transform.position + randomPointOnSphere(50.0f), Quaternion.identity);
-			enemy.GetComponent<EnemyShipController>().Player = Player;
-			enemyList[i] = enemy;
+	// Update is called once per frame
+	void Update () {
+		//spawn enemies
+		spawnTimer += Time.deltaTime;
+		while (spawnTimer >= SpawnInterval) {
+			spawnTimer -= SpawnInterval;
+			
 		}
+		//update GUI
+		GUI.souls.text = totalSoulScore.ToString("D4");
+		MechController mech = Player.GetComponent<MechController> ();
+		float health = (float)mech.health / (float)mech.getMaxHealth();
+		float shield = (float)mech.shield / (float)mech.getMaxShield();
+		GUI.health.sizeDelta = new Vector2(GUI.getHealthWidth() * health, GUI.health.rect.height);
+		GUI.shield.sizeDelta = new Vector2(GUI.getShieldWidth() * shield, GUI.shield.rect.height);
 	}
 
 	void createAsteroidField (){
@@ -67,19 +114,6 @@ public class GameController : MonoBehaviour {
 			asteroid3.transform.localScale = new Vector3(magnitudeX, magnitudeY, magnitudeZ);
 			
 		}
-
-
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		GUI.souls.text = totalSoulScore.ToString("D4");
-		MechController mech = Player.GetComponent<MechController> ();
-		float health = (float)mech.health / (float)mech.getMaxHealth();
-		float shield = (float)mech.shield / (float)mech.getMaxShield();
-		GUI.health.sizeDelta = new Vector2(GUI.getHealthWidth() * health, GUI.health.rect.height);
-		GUI.shield.sizeDelta = new Vector2(GUI.getShieldWidth() * shield, GUI.shield.rect.height);
 	}
 
 	public static Vector3 randomPointOnSphere(float radius) {
@@ -100,5 +134,48 @@ public class GameController : MonoBehaviour {
 
 	public GameObject getPlayer() {
 		return Player.gameObject;
+	}
+	
+	private class Wave {
+		private GameController Control;
+		private int Num;
+		private int[] Enemies;
+		private EZObjectPool[] Pools;
+
+		public Wave(GameController control, int num, int[] enemies) {
+			if (enemies.Length != 6) throw new UnityException("Must have 6 enemy types!");
+			Control = control;
+			Enemies = enemies;
+			Num = num;
+		}
+
+		~Wave() {
+			destroyPools();
+		}
+
+		private void createPools() {
+			Pools[Enemy.Ship1] = EZObjectPool.CreateObjectPool(Control.EnemyShip1Prefab, "SHIP1_WAVE"+Num, Enemies[Enemy.Ship1], false, true, false);
+			Pools[Enemy.Ship2] = EZObjectPool.CreateObjectPool(Control.EnemyShip2Prefab, "SHIP2_WAVE"+Num, Enemies[Enemy.Ship2], false, true, false);
+			Pools[Enemy.Ship3] = EZObjectPool.CreateObjectPool(Control.EnemyShip3Prefab, "SHIP3_WAVE"+Num, Enemies[Enemy.Ship3], false, true, false);
+			Pools[Enemy.Mech1] = EZObjectPool.CreateObjectPool(Control.EnemyMech1Prefab, "MECH1_WAVE"+Num, Enemies[Enemy.Mech1], false, true, false);
+			Pools[Enemy.Mech2] = EZObjectPool.CreateObjectPool(Control.EnemyMech2Prefab, "MECH2_WAVE"+Num, Enemies[Enemy.Mech2], false, true, false);
+			Pools[Enemy.Mech3] = EZObjectPool.CreateObjectPool(Control.EnemyMech3Prefab, "MECH3_WAVE"+Num, Enemies[Enemy.Mech3], false, true, false);
+		}
+
+		private void destroyPools() {
+			for (int i=0; i<Pools.Length; i++) {
+				Pools[i].DeletePool(true);
+			}
+		}
+		
+		public void spawnNext() {
+			GameObject obj = null;
+			for (int i=0; i<Pools.Length; i++) {
+				if (Pools[i].TryGetNextObject(Control.Player.transform.position + randomPointOnSphere(50.0f), Quaternion.identity, out obj)) {
+					obj.GetComponent<EnemyShipController>().Player = Control.Player;
+					return;
+				}
+			}
+		}
 	}
 }
