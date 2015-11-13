aa3980effba1247ee5c5fb66e17acb5b794153f5
Kamil Remiszewski <kamil.remiszewski@stonybrook.edu>
2015-11-13 14:06:03 -0500

    updated sword slash controller

---
+++ Assets/Scrips/SwordController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordController : MonoBehaviour {
+
+	public int swordDamage = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Enemy"))
+		{
+			other.gameObject.GetComponent<EnemyController>().applyDamage(swordDamage);
+		}
+	}
+}
